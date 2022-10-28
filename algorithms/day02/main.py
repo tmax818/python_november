4a556ec6f719fde5601ac7486de8ff002f7d6faa
tmax818 <tylermaxwell661@gmail.com>
2022-10-28 07:26:43 -0700

    algo update

---
+++ algorithms/day02/main.py
@@ -0,0 +1,23 @@
+# /* case insensitive string compare */
+
+strA1 = "ABC"
+strB1 = "abc"
+expected1 = True
+
+strA2 = "ABC"
+strB2 = "abd"
+expected2 = False
+
+strA3 = "ABC"
+strB3 = "bc"
+expected3 = False
+
+
+#  * Determines whether or not the strings are equal, ignoring case.
+#  * - Time: O(?).
+#  * - Space: O(?).
+#  * @param {string} strA
+#  * @param {string} strB
+#  * @returns {boolean} If the strings are equal or not.
+def caseInsensitiveStringCompare(strA, strB):
+    pass
