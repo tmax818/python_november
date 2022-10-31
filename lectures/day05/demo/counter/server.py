817e2861f5a29412b1c6774cc9d62b9fd7edc9dc
tmax818 <tylermaxwell661@gmail.com>
2022-10-31 10:15:32 -0700

    add counter demo

---
+++ lectures/day05/demo/counter/server.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, session, redirect
+
+app = Flask(__name__)
+app.secret_key = "anything"
+
+@app.route('/')
+def index():
+    if 'count' in session:
+        session['count'] += 1
+    else:
+        session['count'] = 1
+    return render_template('index.html')
+
+@app.route('/reset')
+def reset():
+    session.clear()
+    return redirect('/')
+
+if __name__ == "__main__":
+    app.run(debug=True)
