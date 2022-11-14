26231024269f7d728767e4dcb02e39a0a9125341
tmax818 <tylermaxwell661@gmail.com>
2022-11-14 09:45:30 -0800

    update

--- algorithms/day09/index.js
+++ algorithms/day09/index.js
@@ -4,7 +4,8 @@
   and rotates the characters in the string to the right by that given
   integer amount.
 */
-
+//           0,1,2,3,4,5,6,7,8,9,10
+//           H,e,l,l,o, ,W,o,r,l,d
 const str = "Hello World";
 
 const rotateAmnt1 = 0;
@@ -21,6 +22,9 @@ const expected4 = "orldHello W";
 
 const rotateAmnt5 = 13;
 const expected5 = "ldHello Wor";
+
+const rotateAmnt6 = 24;
+const expected6 = "ldHello Wor";
 /* 
 Explanation: this is 2 more than the length so it ends up being the same
 as rotating it 2 characters because after rotating every letter it gets back
@@ -37,4 +41,24 @@ to the original position.
  *    right.
  * @returns {string} The string rotated by the given amount.
  */
-function rotateStr(str, amnt) {}
+function rotateStr(str, amnt) {
+    let newStr = ""
+    // amnt = amnt % str.length
+    // get the first part of the new string
+    for(let i = str.length - (amnt % str.length); i < str.length; i++){
+        newStr += str[i]
+    }
+    for(let i = 0; i < str.length - (amnt % str.length); i++){
+        newStr += str[i]
+    }
+    console.log(newStr)
+    // add it to the new string
+    // iterate again and stop before we get to the begining of the first part.
+}
+
+rotateStr(str, rotateAmnt1)
+rotateStr(str, rotateAmnt2)
+rotateStr(str, rotateAmnt3)
+rotateStr(str, rotateAmnt4)
+rotateStr(str, rotateAmnt5)
+rotateStr(str, rotateAmnt6)
