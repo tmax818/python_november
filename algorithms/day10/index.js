efed54df75610575851073330615ab7953b6678b
tmax818 <tylermaxwell661@gmail.com>
2022-11-16 08:44:42 -0800

    update

--- algorithms/day10/index.js
+++ algorithms/day10/index.js
@@ -2,8 +2,19 @@
   Create the function isRotation(str1,str2) that
   returns whether the second string is a rotation of the first.
 */
+function rotateStr(str, amnt) {
+  let newStr = ""
+  for(let i = str.length - (amnt % str.length); i < str.length; i++){
+      newStr += str[i]
+  }
+  for(let i = 0; i < str.length - (amnt % str.length); i++){
+      newStr += str[i]
+  }
+  return newStr
 
-const strA1 = "ABCD";
+}
+
+const strA1 = "ABCDABCD"; 
 const strB1 = "CDAB";
 // Explanation: if you start from A in the 2nd string, the letters are in the same order, just rotated
 const expected1 = true;
@@ -26,4 +37,26 @@ const expected3 = false;
  * @param {string} s2
  * @returns {boolean} Whether the second string is a rotated version of the 1st.
  */
-function isRotation(s1, s2) {}
+
+function cleverSolution(s1, s2){
+  let compareStr = s1 + s1
+  return compareStr.includes(s2)
+}
+
+function isRotation(s1, s2) {
+  if(s1.length !== s2.length){
+    return false
+  }
+  for(let i = 1; i <= s1.length; i++){
+    // console.log(i)
+    // console.log(rotateStr(s1, i))
+    if(rotateStr(s1,i) === s2){
+      return true
+    }
+  }
+  return false
+}
+
+console.log(isRotation(strA1, strB1))
+console.log(isRotation(strA2, strB2))
+console.log(isRotation(strA3, strB3))
