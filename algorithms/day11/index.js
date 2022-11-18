abc02cc4117c091f7eb62f18a507ef9330f99c11
tmax818 <tylermaxwell661@gmail.com>
2022-11-17 16:37:41 -0800

    update

--- algorithms/day11/index.js
+++ algorithms/day11/index.js
@@ -5,10 +5,13 @@ that space,
 return whether or not there is at least 6 feet separating every person.
 Bonus: O(n) linear time (avoid nested loops that cause re-visiting indexes).
 */
+// if we see a one, check to see if we have seen a one before.
+// if we have, we need to check the number of zeros between that one and the previons
 
+//0             0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11
 const queue1 = [0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1];
 const expected1 = false;
-
+//              0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15,16,17,18
 const queue2 = [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1];
 const expected2 = true;
 
@@ -26,4 +29,23 @@ const expected4 = true;
  * @param {Array<0|1>} queue
  * @returns {Boolean}
  */
-function socialDistancingEnforcer(queue) {}
+function socialDistancingEnforcer(queue) {
+    let indexArr = [];
+    for(let i = 0; i < queue.length; i++){
+        if(queue[i]){
+            indexArr.push(i)
+        }
+    }
+    for(let i = 0; i < indexArr.length; i++){
+        if(indexArr[i+1] - indexArr[i] < 7){
+            return false
+        }
+    }
+    return true
+
+}
+
+console.log(socialDistancingEnforcer(queue1))
+console.log(socialDistancingEnforcer(queue2))
+console.log(socialDistancingEnforcer(queue3))
+console.log(socialDistancingEnforcer(queue4))
